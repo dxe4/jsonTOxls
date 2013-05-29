280f0237d2088b99e823deae5bba0f6d50ebe3d5
charalambosp <papalouizoucharalambos@rentalcars.com>
2013-05-29 22:20:54 +0100

    Packages and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='jsonTOxls',
+    version='0.01',
+    packages=['client','server'],
+    license='GPL',
+    long_description=open('README.md').read(),
+)
