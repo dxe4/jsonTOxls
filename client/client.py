fcf037c90cc539d3833943c8b104af2b943055f8
charalambosp <papaloizouc@gmail.com>
2013-06-02 22:46:19 +0100

    Re Organized examples

--- client/client.py
+++ client/client.py
@@ -5,7 +5,7 @@ import examples
 data = {'message':'hello world!', 'test':123.4}
 url = 'http://localhost:12345/'
 headers = {'content-type': 'application/json'}
-sheet = examples.formats()
+sheet = examples.example2_formats_simple()
 response = requests.post(url, data=json.dumps(sheet), headers=headers)
 
 print(response.text)
--- client/examples.py
+++ client/examples.py
@@ -1,6 +1,6 @@
 import datetime
 
-def hello_world():
+def example1_hello_world():
     sheet_data = {
         'A1':'Item',
         'B1':'Cost',
@@ -25,7 +25,41 @@ def hello_world():
     }
     return sheet
 
-def formats():
+def example2_formats_simple():
+    sheet_data = {
+        'A1':{'value':'Item','format':'bold'},
+        'B1':{'value':'Cost','format':'bold'},
+         #
+        '1,0':'Rent',
+        '2,0':'Gas',
+        '3,0':'Food',
+        '4,0':'Gym',
+        '5,0':'Total',
+         #
+        '1,1':{'value':'500.50','format':'number_bold_red'},
+        '2,1':{'value':'150.88','format':'number_bold_red'},
+        '3,1':{'value':'330.90','format':'number_bold_red'},
+        '4,1':{'value':'80.55','format':'number_bold_red'},
+        '5,1':{'value':'=SUM(B2:B5)','format':'number_bold_red'}
+    }
+
+    sheet = {
+        'sheets':[
+            {"Test Sheet" :  sheet_data},
+        ],
+        "formats" : {
+            'bold' : {'bold':True},
+            'bold_red' : {'bold': True, 'font_color': 'red'},
+            #
+            'number' : {'num_format':'$#,##.##'} ,
+            'number_bold_red' : {'bold': True, 'font_color': 'red','num_format':'$#,##.##'},
+            'number_bold_blue' : {'bold': True, 'font_color': 'red','bg_color': '#99CCFF','num_format':'$#,##.##'},
+
+        }
+    }
+    return sheet
+
+def example3_formats_more():
     sheet_data = {
         'A1':{'value':'Item','format':'bold'},
         'B1':{'value':'Cost','format':'bold'},
