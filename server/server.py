4ed96f9895479daa23001781a65b645d3b9301eb
charalambosp <papalouizoucharalambos@rentalcars.com>
2013-06-02 21:00:16 +0100

    return the exception to the client in case an exception was thrown.

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 import tornado.ioloop
 import tornado.web
 from tornado.escape import json_decode
+import traceback,sys
 import xlsx_factory
 
 
@@ -8,9 +9,12 @@ class MainHandler(tornado.web.RequestHandler):
     def post(self):
         content_type = self.request.headers.get("Content-Type", "")
         if content_type.startswith("application/json"):
-            input = json_decode(self.request.body)
-            result = xlsx_factory.create(input)
-            self.write(result)
+            try:
+                input = json_decode(self.request.body)
+                result = xlsx_factory.create(input)
+                self.write(result)
+            except Exception as e:
+                self.write("".join(traceback.format_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])))
         else:
             self.write("content_type should be set to application/json")
 
