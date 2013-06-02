24421e4437f4dcd480431e32f97cb304c9be7d84
charalambosp <papalouizoucharalambos@rentalcars.com>
2013-06-02 21:43:03 +0100

    Adden json support for dates

--- client/examples.py
+++ client/examples.py
@@ -1,3 +1,5 @@
+import datetime
+
 def hello_world():
     sheet_data = {
         'A1':'Item',
@@ -25,20 +27,26 @@ def hello_world():
 
 def formats():
     sheet_data = {
-        '0,0':{'value':'Item','format':'bold'},
-        'B1':{'value':'Cost','format':'bold'},
+        'A1':{'value':'Item','format':'bold'},
+        'B1':{'value':'Cost','format':'bold'}, #2013-01-13
+        'C1':{'value':'Date','format':'bold'},
          #
-        'A2':'Rent',
-        'A3':'Gas',
-        'A4':'Food',
-        'A5':'Gym',
-        'A6':'Total',
+        '1,0':'Rent',
+        '2,0':'Gas',
+        '3,0':'Food',
+        '4,0':'Gym',
+        '5,0':'Total',
          #
-        'B2':{'value':'500.50','format':'number_bold_red'},
-        'B3':{'value':'150.88','format':'number_bold_red'},
-        'B4':{'value':'330.90','format':'number_bold_red'},
-        'B5':{'value':'80.55','format':'number_bold_red'},
-        'B6':{'value':'=SUM(B2:B5)','format':'sum_format'},
+        '1,1':{'value':'500.50','format':'number_bold_red'},
+        '2,1':{'value':'150.88','format':'number_bold_red'},
+        '3,1':{'value':'330.90','format':'number_bold_red'},
+        '4,1':{'value':'80.55','format':'number_bold_red'},
+        '5,1':{'value':'=SUM(B2:B5)','format':'sum_format'},
+
+        '1,2':{'date':'2013-01-01','format':'date_format'},
+        '2,2':{'date':'2013-02-01','format':'date_format'},
+        '3,2':{'date':'2013-03-01','format':'date_format2'},
+        '4,2':{'date':'2013-04-01','format':'date_format2'},
 
         "conditional_formats" : {
             'B2:B5': {
@@ -46,7 +54,8 @@ def formats():
             }
         },
         "column_size":{
-             'A:B': 15
+             'A:B': 15,
+             'C:C':25
         }
     }
 
@@ -60,7 +69,9 @@ def formats():
             'bold_red' : {'bold': True, 'font_color': 'red'},
             'number_bold_red' : {'bold': True, 'font_color': 'red','num_format':'$#,##.##'},
             'number_bold_blue' : {'bold': True, 'font_color': 'red','bg_color': '#99CCFF','num_format':'$#,##.##'},
-            'sum_format' : {'bold':True,'bg_color':'#E9AA94'}
+            'sum_format' : {'bold':True,'bg_color':'#E9AA94'},
+            'date_format':{'num_format': 'yyyy d mmmm'},
+            'date_format2':{'num_format': 'yy dd mmm'}
         }
     }
     return sheet
