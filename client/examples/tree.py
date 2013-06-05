b50b1c602887a48aebb8ef27177ab7e998667f30
charalambosp <papaloizouc@gmail.com>
2013-06-05 06:44:22 +0100

    added parent on nodes and tidied up the whitespaces

--- client/examples/tree.py
+++ client/examples/tree.py
@@ -1,15 +1,20 @@
 class Node:
+
     def __init__(self,info=None,children=[],parent=None,data = None):
         self.info = data if not info else info
         self.children = children
         self.parent = parent
         self.data = data
+
     def __str__(self):
         return "info:%s children:%s parent: %s data:%s" % (self.info, self.children, self.parent,self.data)
+
     def __repr__(self):
         return str(self.info)
 
+
 class Tree:
+
     def __init__(self,root=Node()):
         self.root = root
 
@@ -17,9 +22,8 @@ class Tree:
         return self.root
 
 
-
 if __name__ == "__main__":
     tree = Tree()
     for i in range(0,10):
-        tree.root.children.append(Node(data = i))
+        tree.root.children.append(Node(data = i,parent=tree.root))
     print(tree.root)
