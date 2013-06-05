8f518972f631542b9ac684f8e920dc0c33ac5ffc
charalambosp <papaloizouc@gmail.com>
2013-06-05 06:24:11 +0100

    changed node.info to default to node.data if None

--- client/examples/tree.py
+++ client/examples/tree.py
@@ -1,13 +1,13 @@
 class Node:
     def __init__(self,info=None,children=[],parent=None,data = None):
-        self.info = info
+        self.info = data if not info else info
         self.children = children
         self.parent = parent
         self.data = data
     def __str__(self):
         return "info:%s children:%s parent: %s data:%s" % (self.info, self.children, self.parent,self.data)
     def __repr__(self):
-        return "data:%s " % (self.data,)
+        return str(self.info)
 
 class Tree:
     def __init__(self,root=Node()):
