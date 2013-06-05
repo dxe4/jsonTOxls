1d2d9436aaf659a0c2971f3b63a9925e587ab0af
charalambosp <papaloizouc@gmail.com>
2013-06-05 06:16:34 +0100

    added tree, useful for reports

---
+++ client/examples/tree.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,info=None,children=[],parent=None,data = None):
+        self.info = info
+        self.children = children
+        self.parent = parent
+        self.data = data
+    def __str__(self):
+        return "info:%s children:%s parent: %s data:%s" % (self.info, self.children, self.parent,self.data)
+    def __repr__(self):
+        return "data:%s " % (self.data,)
+
+class Tree:
+    def __init__(self,root=Node()):
+        self.root = root
+
+    def __str__(self):
+        return self.root
+
+
+
+if __name__ == "__main__":
+    tree = Tree()
+    for i in range(0,10):
+        tree.root.children.append(Node(data = i))
+    print(tree.root)
