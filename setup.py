8e8861697c98e0888f18f59a7e94811327935c7f
charalambosp <papaloizouc@gmail.com>
2013-06-09 01:15:06 +0100

    python3 setup.py sdist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='jsonTOxls',
     version='0.01',
-    packages=['client','server'],
+    packages=['client','server','client/examples'],
     license='GPL',
     long_description=open('README.md').read(),
 )
