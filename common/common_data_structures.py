8fe7506db5f7c830b9bb4f583d56254916520ca9
charalambosp <papaloizouc@gmail.com>
2013-06-15 17:44:40 +0100

    Added common package

---
+++ common/common_data_structures.py
@@ -0,0 +1,10 @@
+import random
+
+def random_list_item(list):
+    """
+        Selects a random item from the given list
+    :param list: list to select from
+    :return: random item from list
+    """
+    index = random.randrange(0,list.__len__())
+    return list[index]
---
+++ common/common_date.py
@@ -0,0 +1,36 @@
+import datetime
+import random
+
+#TODO fix random days
+def random_date(year=None,month=None,day=None):
+    """
+        Creates a random date.
+        WARNING: random days = 1-28 (29-31 wont get generated for now)
+    :param year: if value passed wont be random
+    :param month: if value passed wont be random
+    :param day: if value passed wont be random
+    """
+    month = month if month else random.randrange(1,12)
+    day = day if day else random.randrange(1,28)
+    year = year if year else random.randrange(2013,2014)
+    print(year,month,day)
+    return datetime.date(year,month, day)
+
+#TODO fix random days
+def random_dates_sorted(year=None,month=None,day=None):
+    """
+        Creates a 2 random dates, and returns them sorted.
+        WARNING: random days = 1-28 (29-31 wont get generated for now)
+    :param year: if value passed wont be random
+    :param month: if value passed wont be random
+    :param day: if value passed wont be random
+    :return: date_1 date_2 where date1 is before date_2
+    """
+    date_1 = random_date(year=year,month=month,day=day)
+    date_2 = random_date(year=year,month=month,day=day)
+    if date_1 if date_1 < date_2 else date_2:
+        return date_1,date_2
+    else:
+        return date_2,date_1
+
+
