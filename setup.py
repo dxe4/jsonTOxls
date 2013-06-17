395c3d345b3445fe38be3cf9178521b41d79eed3
charalambosp <papaloizouc@gmail.com>
2013-06-17 23:12:13 +0100

    Changed setup.py and run: python3 setup.py sdist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='jsonTOxls',
     version='0.01',
-    packages=['client','server','client/examples'],
+    packages=['client','server','client/examples','common'],
     license='GPL',
     long_description=open('README.md').read(),
 )
