65e542f66304885724fb7d077698f4f8d64287fb
charalambosp <papaloizouc@gmail.com>
2013-06-17 23:35:11 +0100

    Moved example var to the top and added documentation

--- client/client.py
+++ client/client.py
@@ -2,11 +2,12 @@ import json
 import requests
 from examples import example_functions
 
+#change to 1,2,3,4 for different examples
+example = "4"
 
 url = 'http://localhost:12345/'
 headers = {'content-type': 'application/json'}
 
-example = "4"
 data =  example_functions.functions[example]
 data["filename"] = "example"+example+".xls"
 response = requests.post(url, data=json.dumps(data), headers=headers)
