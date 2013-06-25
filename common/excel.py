a28025ad9ca1148e277c83960c0bee08b758b07f
charalambosp <papaloizouc@gmail.com>
2013-06-25 18:21:47 +0100

    Added upper for char

--- common/excel.py
+++ common/excel.py
@@ -2,15 +2,15 @@ a_to_z = range(ord("a"), ord("z"))
 
 
 def number_to_cell(row, col):
-    new_row = chr(a_to_z[col])
+    new_row = chr(a_to_z[col]).upper()
     return new_row.upper() + str(row)
 
 
 def number_to_cell(col):
-    return chr(a_to_z[col])
+    return chr(a_to_z[col]).upper()
 
 
 def number_to_cells(columns):
-    return [chr(a_to_z[col]) for col in columns]
+    return [chr(a_to_z[col]).upper() for col in columns]
 
 
