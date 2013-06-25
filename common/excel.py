c2a6bc3d95a93502a55f7b296320b2c801f38f90
charalambosp <papaloizouc@gmail.com>
2013-06-25 18:16:50 +0100

    extended number_to_cell functionality

--- common/excel.py
+++ common/excel.py
@@ -2,7 +2,15 @@ a_to_z = range(ord("a"), ord("z"))
 
 
 def number_to_cell(row, col):
-    new_row = chr(a_to_z[row])
-    return new_row.upper() + str(col)
+    new_row = chr(a_to_z[col])
+    return new_row.upper() + str(row)
+
+
+def number_to_cell(col):
+    return chr(a_to_z[col])
+
+
+def number_to_cells(columns):
+    return [chr(a_to_z[col]) for col in columns]
 
 
